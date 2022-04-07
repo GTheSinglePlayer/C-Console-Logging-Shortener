d5a2dbf1f62a033378253c4ce22516eee9657bb6
GTheSinglePlayer <103208866+GTheSinglePlayer@users.noreply.github.com>
2022-04-07 13:47:34 -0300

    Version 0.1 of C++ Printing Utility
     Only "print()" function is avaible.

---
+++ print.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream> // This '.h' file is just a reduced version of iostream console functions.
+
+
+// Your code doesn't need to include iostream :)
+
+
+int print(std::string String) {  // Usage: print(string)
+	std::cout << String&&"\n";
+	
+	return 0x0;
+}
+// I'm planning to add some updates later. Thx for trying out :)
